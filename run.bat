5bce6d5cadc811a6a2c7efb481c428df12f65e79
Brendan R. Becker <Brendan.R.Becker>
2016-07-24 13:18:53 -0400

    APIKey and creds

--- run.bat
+++ run.bat
@@ -1,9 +1,6 @@
 cd ./web
 start cmd /k python -m SimpleHTTPServer 8000
 @echo off
-set /p Username= Username-
-set /p Password= Password-
-set /p UserInputPath= Set Location-
 cd ..
-python main.py -u %Username% -p %Password% -l "%UserInputPath%"
+python main.py -u BeckerAPI -p EasyPass123 -l "1000 Barrie Ave Flint, MI 48507"
 pause
